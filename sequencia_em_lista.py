b33fbcb8deb808b0024e85b0ac94699e67cc3e2b
Riverfount <vicente.marcal@gmail.com>
2017-04-10 22:39:22 -0300

    Refatorando e colocando o código numa função

--- sequencia_em_lista.py
+++ sequencia_em_lista.py
@@ -1,17 +1,24 @@
-lista = [1, 2, 3, 4, 9, 10, 11, 12, 13, 14, 21, 22, 23, 24]
-lista_resultante = []
-lista_intermediaria = []
+def maior_sequencia(lista):
 
-for i in range(len(lista)):
-    if i == 0 or (lista[i] == lista[i - 1] + 1):
-        lista_intermediaria.append(lista[i])
-    else:
-        if len(lista_intermediaria) > len(lista_resultante):
-            lista_resultante = lista_intermediaria
-            lista_intermediaria = []
+    lista_resultante = []
+    lista_intermediaria = []
+    for i in range(len(lista)):
+        if i == 0 or (lista[i] == lista[i - 1] + 1):
             lista_intermediaria.append(lista[i])
+        else:
+            if len(lista_intermediaria) > len(lista_resultante):
+                lista_resultante = lista_intermediaria
+                lista_intermediaria = []
+                lista_intermediaria.append(lista[i])
+            else:
+                lista_intermediaria = []
+                lista_intermediaria.append(lista[i])
+    if len(lista_intermediaria) > len(lista_resultante):
+        lista_resultante = lista_intermediaria
 
-if len(lista_intermediaria) > len(lista_resultante):
-    lista_resultante = lista_intermediaria
+    return lista_resultante
 
-print(lista_resultante)
+
+if __name__ == '__main__':
+    lista = [1, 2, 3, 4, 9, 10, 11, 12, 13, 14, 21, 22, 23, 24, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
+    print(maior_sequencia(lista))
