4664fb58d85c477ff032929dd259d6b75322abc7
Vicente E. R. Marçal <vicente.marcal@gmail.com>
2017-05-02 22:38:42 -0300

    Acertos de PEP8

--- average.py
+++ average.py
@@ -5,9 +5,9 @@ Versão: 1.0
 Data: 08 de fevereiro de 2017
 """
 # Entra com o valor das quatro notas
-Nota01 = int(input("Digite a primeira nota: ")) 
-Nota02 = int(input("Digite a segunda nota: ")) 
-Nota03 = int(input("Digite a terceira nota: ")) 
+Nota01 = int(input("Digite a primeira nota: "))
+Nota02 = int(input("Digite a segunda nota: "))
+Nota03 = int(input("Digite a terceira nota: "))
 Nota04 = int(input("Digite a quarta nota: "))
 
 # Calcula a média
@@ -15,4 +15,3 @@ Media = ((Nota01 + Nota02 + Nota03 + Nota04) / 4)
 
 # Imprime o resultado
 print("A média aritmética é", Media)
-
