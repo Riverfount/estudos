3e7dd415149955aa4cce59de892a5928b2fd9e61
Vicente E. R. Marçal <vicente.marcal@gmail.com>
2017-07-27 14:45:34 -0300

    Confere resultado da Lotomania

---
+++ lotomania/lotomania.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+html = requests.request('GET', 'http://loterias.caixa.gov.br/wps/portal/loterias/landing/lotomania/')
+
+bsObj = BeautifulSoup(html.text, 'html.parser')
+
+jogoVicente = ['07', '08', '09', '10', '13', '14', '15', '16', '17', '21', '22', '23', '24', '31', '32', '33',
+               '39', '40', '45', '46', '48', '52', '53', '54', '56', '57', '58', '59', '60', '62', '63', '64',
+               '66', '67', '72', '74', '75', '76', '77', '83', '84', '85', '86', '87', '91', '92', '93', '94',
+               '95', '00']
+
+nums_sorteados = []
+resultado = 0
+cells = []
+
+table = bsObj.table
+
+divJogo = bsObj.find('div', {'class': 'title-bar clearfix'})
+
+for row in table.findAll('tr'):
+    cells.append(row.findAll('td'))
+
+for cell in cells:
+    for i in range(len(cell)):
+        nums_sorteados.append(str(cell[i])[4:6])
+
+for num in jogoVicente:
+    if num in nums_sorteados:
+        resultado += 1
+
+
+concurso = str(divJogo.h2).split()
+concurso_num = concurso[4]
+concurso_data = str(concurso[5])[1:11]
+
+print(f'Concurso: {concurso_num} de {concurso_data}')
+print(f'O jogo do Vicente acertou {resultado} números')
