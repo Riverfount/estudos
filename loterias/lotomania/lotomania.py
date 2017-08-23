22796afa96e33487f7e087575923d1b34824da3a
Vicente Marçal <vicente.marcal@gmail.com>
2017-08-22 23:07:12 -0300

    Acertando posicionamento

--- loterias/lotomania/lotomania.py
+++ loterias/lotomania/lotomania.py
@@ -10,20 +10,8 @@ jogoVicente = ['07', '08', '09', '10', '13', '14', '15', '16', '17', '21', '22',
                '66', '67', '72', '74', '75', '76', '77', '83', '84', '85', '86', '87', '91', '92', '93', '94',
                '95', '00']
 
-jogoSileis1 = ['01', '04', '06', '08', '20', '22', '26', '29', '31', '33', '40', '43', '49', '50', '51', '52',
-               '54', '55', '57', '58', '60', '61', '63', '65', '66', '69', '70', '72', '73', '74', '76', '77',
-               '78', '79', '80', '81', '82', '84', '85', '86', '87', '89', '90', '91', '92', '94', '95', '97',
-               '98', '99']
-
-jogoSileis2 = ['02', '03', '05', '07', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '21',
-               '23', '24', '25', '27', '28', '30', '32', '34', '35', '36', '37', '38', '39', '41', '42', '44',
-               '45', '46', '47', '48', '53', '56', '59', '62', '64', '67', '68', '71', '75', '83', '88', '93',
-               '96', '00']
-
 nums_sorteados = []
 res_vic = 0
-res_sileis1 = 0
-res_sileis2 = 0
 cells = []
 
 table = bsObj.table
@@ -45,22 +33,11 @@ for num in jogoVicente:
     if num in nums_sorteados:
         res_vic += 1
 
-for num in jogoSileis1:
-    if num in nums_sorteados:
-        res_sileis1 += 1
-
-for num in jogoSileis2:
-    if num in nums_sorteados:
-        res_sileis2 += 1
-
-print('=' * 42)
-print(f'Concurso: {concurso_num} de {concurso_data}')
+print('\n\n\n'+ '=' * 42)
+print(f'Concurso: {concurso_num} de {concurso_data}'.center(42))
 print('=' * 42 + '\n')
-print(f'Vicente acertou {res_vic} números')
+print('-' * 42)
+print(f' Vicente acertou {res_vic} números '.center(42))
 print('-' * 42 + '\n')
-print(f'Sileis acertou {res_sileis1} números, no primeiro jogo')
-print('-' * 42 + '\n')
-print(f'Sileis acertou {res_sileis2} números, no segundo jogo')
-print('=' * 42 + '\n')
-
+print('=' * 42 + '\n\n\n')
 
