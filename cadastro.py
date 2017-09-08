e35f9e36fbfc7910e5f0596e8f6ed31d1b73ec16
Vicente Marçal <vicente.marcal@gmail.com>
2017-09-08 20:48:01 -0300

    Scritp de estudos com o Cristiano

---
+++ cadastro.py
@@ -0,0 +1,14 @@
+# Um exemplo simples de cadastro
+
+nome = input("Qual o seu nome: ")
+email = input("Qual o seu e-mail: ")
+phone = input("Qual o seu telefone: ")
+
+
+print('=' * 45)
+print()
+print('O nome digitado foi %s' % nome)
+print('O email digitado foi %s' % email)
+print('O telefone digitado foi %s' % phone)
+print()
+print('=' * 45)
